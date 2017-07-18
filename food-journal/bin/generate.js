ef5850b011f7a9bea7b1c6bdf8f73574370f74f6
Stanislav Tugovikov <s.tugovikov@corp.mail.ru>
2017-07-18 23:52:05 +0300

    remove generate

--- food-journal/bin/generate.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-const fs = require('fs');
-const list = fs.readdirSync('photos').filter(i => !i.match(/_thumb/)).sort().reverse();
-
-fs.writeFileSync('list.js', 'var list = ' + JSON.stringify(list,null,"\t"), 'utf-8');
